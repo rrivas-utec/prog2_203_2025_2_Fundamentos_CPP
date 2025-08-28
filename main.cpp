e31c5e2137b8e7559014a05d47fa64954c949a72
rrivas <rrivas@utec.edu.pe>
2025-08-28 17:53:22 -0500

    actualizado

--- main.cpp
+++ main.cpp
@@ -39,12 +39,53 @@ void ejercicio_1() {
 }
 
 void ejercicio_2() {
+    int a, b, c;
+    cout << "ingrese 3 valores: ";
+    cin >> a >> b >> c;
 
+    if (a > b) {
+        const int temp = a;
+        a = b;
+        b = temp;
+    }
+    if (a > c) {
+        const int temp = a;
+        a = c;
+        c = temp;
+    }
+    if (b > c) {
+        const int temp = b;
+        b = c;
+        c = temp;
+    }
+    cout << "Los valores son: " << a << " " << b << " " << c << endl;
+}
+
+void ejercicio_3() {
+    int a, b, c;
+    cout << "ingrese 3 valores: ";
+    cin >> a >> b >> c;
+
+    bool es_triangulo = (a + b > c && a + c > b && b + c > a);
+    if (es_triangulo) {
+        if (a == b && a == c) {
+            cout << "Triangulo equilatero" << endl;
+        }
+        if (a == b && a != c) {
+            cout << "Triangulo isoceles" << endl;
+        }
+        if (a != b && a != c && b != c) {
+            cout << "Triangulo escaleno" << endl;
+        }
+    }
+    else {
+        cout << "Invalido, no es triangulo" << endl;
+    }
 }
 
 int main() {
     // Llamada de funcion ejercicio_1
-    ejercicio_1();
+    // ejercicio_1();
     // ejercicio_2();
     return 0;
 }
