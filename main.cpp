1b529f0724545888283211d146fcbc2e4015d7d2
rrivas <rrivas@utec.edu.pe>
2025-08-29 17:32:52 -0500

    Agregar Ejercicio 11 y Ejercicio 12

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cctype>
 using namespace std;
 
 void ejercicio_1() {
@@ -82,6 +83,79 @@ void ejercicio_3() {
         cout << "Invalido, no es triangulo" << endl;
     }
 }
+/*
+$1: Presenta el retrato de George Washington.
+$2: Muestra el retrato de Thomas Jefferson.
+$5: Presenta a Abraham Lincoln.
+$10: Muestra a Alexander Hamilton.
+$20: Presenta a Andrew Jackson.
+$50: Muestra a Ulysses S. Grant.
+$100: Presenta a Benjamin Franklin.
+*/
+void ejercicio_11() {
+    // 1. Definir las variables
+    int importe = 0;
+
+    // 2. Solicitar el importe (cout / cin)
+    cout << "ingrese el importe de billete: ";
+    cin >> importe;
+
+    // 3. Verificar si existe un billete (switch)
+    switch (importe) {
+        case 1:
+            cout << "El billete contiene la imagen de George Washington\n";
+        break;
+        case 2:
+            cout << "El billete contiene la imagen de Thomas Jefferson";
+        break;
+        case 5:
+            cout << "El billete contiene la imagen de Abraham Lincoln";
+        break;
+        case 500:
+        case 1000:
+        case 5000:
+        case 10000:
+            cout << "Billete Descontinuado\n";
+        default:
+            cout << "Billete Invalido\n";
+    }
+}
+
+void ejercicio_12() {
+    // 1. declarar las variables tipo char
+    char color = 0;
+    // 2. Ingresar el valor
+    cout << "ingrese la letra inicial del color: ";
+    cin >> color;
+    // 3. Convertir a mayuscula
+    // color = toupper(color);
+    // 4. Seleccionar el color usando switch
+    switch (color) {
+        case 'a':
+        case 'A':
+            cout << "El color seleccionado es Azul\n";
+        break;
+        case 'r':
+        case 'R':
+            cout << "El color seleccionado es Rojo\n";
+        break;
+        case 'm':
+        case 'M':
+            cout << "El color seleccionado es Amarillo\n";
+        break;
+        case 'b':
+        case 'B':
+            cout << "El color seleccionado es Blanco\n";
+        break;
+        case 'n':
+        case 'N':
+            cout << "El color seleccionado es Negro\n";
+        break;
+        default:
+            cout << "Color no disponible\n";
+    }
+}
+
 
 int main() {
     // Llamada de funcion ejercicio_1
